19575f89a22d88f9686d2fd138e17ad833f8d6ee
scottmhamilton <scott.hamilton@uantwerpen.be>
2020-04-08 11:10:52 +0200

    updated mongo version, git urls, stanford corenlp version and added install cron

--- Vagrantfile
+++ Vagrantfile
@@ -8,7 +8,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.provider "virtualbox" do |vb|
     vb.customize ["modifyvm", :id, "--memory", "4096"]
   end
-  config.vm.box = "ubuntu/trusty64"
-  config.vm.box_url = "https://vagrantcloud.com/ubuntu/trusty64"
+  config.vm.box = "ubuntu/xenial64"
+  config.vm.box_url = "https://vagrantcloud.com/ubuntu/xenial64"
   config.vm.provision :shell, :path => "bootstrap.sh"
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' |
-tee /etc/apt/sources.list.d/mongodb.list
+sudo apt-get install gnupg
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" |
+sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+
 
 sudo apt-get update
 
@@ -33,9 +35,9 @@ yes
 EOF
 
 echo "Cloning Phoenix pipeline files..."
-sudo git clone https://github.com/openeventdata/phoenix_pipeline.git
-sudo git clone https://github.com/openeventdata/scraper.git
-sudo git clone https://github.com/openeventdata/stanford_pipeline.git
+sudo git clone https://github.com/scottmhamlton/phoenix_pipeline.git
+sudo git clone https://github.com/scottmhamilton/scraper.git
+sudo git clone https://github.com/scottmhamilton/stanford_pipeline.git
 
 echo "Installing Python dependencies..."
 sudo pip install -r scraper/requirements.txt
@@ -43,13 +45,13 @@ sudo pip install -r phoenix_pipeline/requirements.txt
 sudo pip install -r stanford_pipeline/requirements.txt
 
 echo "Installing PETRARCH..."
-sudo pip install git+https://github.com/openeventdata/petrarch.git
+sudo pip install git+https://github.com/scottmhamilton/petrarch2.git
 cd
 
 echo "Downloading CoreNLP..."
-sudo wget http://nlp.stanford.edu/software/stanford-corenlp-full-2014-06-16.zip
-sudo unzip stanford-corenlp-full-2014-06-16.zip
-mv stanford-corenlp-full-2014-06-16 /home/vagrant/stanford-corenlp
+sudo wget http://nlp.stanford.edu/software/stanford-corenlp-full-2018-10-05.zip
+sudo unzip stanford-corenlp-full-2018-10-05.zip
+mv stanford-corenlp-full-2018-10-05 /home/vagrant/stanford-corenlp
 cd /home/vagrant/stanford-corenlp
 echo "Downloading shift-reduce parser..."
 sudo wget http://nlp.stanford.edu/software/stanford-srparser-2014-07-01-models.jar
@@ -63,7 +65,9 @@ cd
 sudo mv nltk_data /usr/lib/nltk_data
 
 echo "Installing MongoDB..."
-sudo apt-get install mongodb-10gen
+sudo apt-get install -y mongodb-org
 
 echo "Setting up crontab..."
+sudo apt update
+sudo apt install cron
 sudo crontab /vagrant/crontab.txt
